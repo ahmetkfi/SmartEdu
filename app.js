57c93f0946dbe1a19239a0e97dfc49f2fb65a363
ahmetkfi <ahmetkcygt58@gmail.com>
2023-05-01 11:52:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.status(200)
+    .send('Index Sayfası');
+});
+
+
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`App stardet on port ${port}`);
+});
+
